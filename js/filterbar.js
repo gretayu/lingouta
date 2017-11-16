f1b0bade08e3b845644f03de02b1a64395a9512c
gretayu <gretayukai@gmail.com>
2017-11-16 22:46:44 +0800

    Update filterbar.js

--- js/filterbar.js
+++ js/filterbar.js
@@ -5,8 +5,8 @@ $(document).ready( function() {
 	    $(this).addClass("active");
 	    $("#filter-bar").removeClass().addClass($(this).attr("data-target"));
 	    
-	    $('#tabs-content .active').removeClass('active'); // remove the class of the corresponding tab content
-        $('.tabs-content #' + $(this).attr("id")).addClass('active'); // add the class to the corresponding tab content
+	    $('#branch-option .active').removeClass('active'); // remove the class of the corresponding tab content
+	    $('.branch-option #' + $(this).attr("id")).addClass('active'); // add the class to the corresponding tab content
     });
 
 })
